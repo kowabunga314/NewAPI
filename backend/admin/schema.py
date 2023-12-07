6cc5bf19e136d7b4b0dd1cc80cf8ee5e2131864c
kowabunga314 <dougfrost314@gmail.com>
2023-12-06 22:58:05 -0600

    Updated docker-compose to postpone startup of api/migration containers until after DB container is up. Added user schema in admin folder.

---
+++ backend/admin/schema.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
+
+
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+
+class UserInDB(UserBase):
+    hashed_password: str
