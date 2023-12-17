45735f41900e339f8f4b3a232cfe3be86509ba1d
kowabunga314 <dougfrost314@gmail.com>
2023-12-16 23:06:29 -0600

    Reset alembic state.

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from database.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,15 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-# target_metadata = [
-#     Product.Base.metadata,
-#     MaterialCost.Base.metadata,
-#     ProductionCost.Base.metadata,
-# ]
-from database.base import Base
-target_metadata = [
-    Base.metadata,
-]
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
