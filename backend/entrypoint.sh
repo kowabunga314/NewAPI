6ef5f35272b03375cd3bc0f2ce24edb19ca41e09
kowabunga314 <dougfrost314@gmail.com>
2023-12-28 23:21:58 -0600

    Added entrypoint command to wait for postgres to become available before starting.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+echo "Testing connection to database..."
+
+while [ $(pg_isready --host=db -q) ]
+do
+    echo "Server is not yet available, waiting..."
+    sleep 2
+done
+
+echo "Server is up."
+
 exec "$@"
